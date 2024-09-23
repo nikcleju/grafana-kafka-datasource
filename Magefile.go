0921c0bb418f7f5bcb386fe862feca61d012eebe
Nicolae Cleju <ncleju@etti.tuiasi.ro>
2024-09-23 19:24:25 +0300

    Update

--- Magefile.go
+++ Magefile.go
@@ -5,9 +5,11 @@ package main
 
 import (
 	"fmt"
-	// mage:import
+	// disable this "/ / mage:import", should be //
 	build "github.com/grafana/grafana-plugin-sdk-go/build"
 )
+
+import	"github.com/magefile/mage/mg"
 
 // Hello prints a message (shows that you can define custom Mage targets).
 func Hello() {
@@ -15,9 +17,23 @@ func Hello() {
 }
 
 // Default configures the default target.
-var Default = build.BuildAll
+var Default = BuildAll
 
 var _ = build.SetBeforeBuildCallback(func(cfg build.Config) (build.Config, error) {
 	cfg.EnableCGo = true
 	return cfg, nil
 })
+
+
+// Nic
+// Build all targets, but only for Linux
+func BuildAll() error {
+
+	// Call the Linux build target programmatically
+	b := build.Build{}
+
+	mg.Deps(b.Linux)
+
+	return nil
+}
+
